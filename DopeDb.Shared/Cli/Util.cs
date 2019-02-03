7a74c0c44f678d6c2d0b6fcd53ee34f3dab4b320
Matthias Klatte <matthias.klatte@gmail.com>
2019-02-03 13:51:27 +0100

    possibility to write objects to the console

--- DopeDb.Shared/Cli/Util.cs
+++ DopeDb.Shared/Cli/Util.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Reflection;
 
-namespace DopeDb.Shared.Cli {
+namespace DopeDb.Shared.Cli
+{
     public class Util
     {
 
@@ -32,7 +34,8 @@ namespace DopeDb.Shared.Cli {
             {
                 Console.Write(' ');
             }
-            if (text.Split("<").Length < 3) {
+            if (text.Split("<").Length < 3)
+            {
                 Console.Write(text);
                 return;
             }
@@ -120,12 +123,24 @@ namespace DopeDb.Shared.Cli {
             Console.ResetColor();
         }
 
+        public static void Write(object obj)
+        {
+            printObject(obj);
+        }
+
         /// <see cref="Util.Write"/>
         /// <param name="text"></param>
         /// <param name="indent">Optionally indent the text</param>
         public static void WriteLine(string text = "", int indent = 0)
         {
             Util.Write(text + '\n', indent);
+            Console.ResetColor();
+        }
+
+        public static void WriteLine(object obj)
+        {
+            printObject(obj);
+            WriteLine();
         }
 
         public static void WriteException(System.Exception exception)
@@ -147,6 +162,79 @@ namespace DopeDb.Shared.Cli {
             catch (System.Exception)
             {
                 return fallback;
+            }
+        }
+
+        protected static void printObject(object obj, int depth = 0)
+        {
+            var indentationWidth = 2;
+            var maxDepth = 3;
+            if (obj == null)
+            {
+                WriteLine(" <fg=Blue>null</>");
+            }
+            else if (obj is bool)
+            {
+                var lbl = (bool)obj ? "true" : "false";
+                WriteLine($" <fg=Blue>{lbl}</>");
+            }
+            else if (obj is string)
+            {
+                WriteLine($" <fg=DarkYellow>\"{obj}\" ({((string)obj).Length})</>");
+            }
+            else if (obj is int || obj is double)
+            {
+                WriteLine($" <fg=DarkMagenta>{obj}</>");
+            }
+            else if (obj.GetType().IsGenericType && obj.GetType().GetGenericTypeDefinition() == typeof(Dictionary<,>))
+            {
+                WriteLine($" <fg=Yellow>{obj.GetType()}</>");
+                foreach (var x in (dynamic)obj)
+                {
+                    var item = (dynamic)x;
+                    Write($"{item.Key} =>", (depth + 1) * indentationWidth);
+                    printObject(item.Value, depth + 2);
+                }
+            }
+            else if (obj is IEnumerable<object>)
+            {
+                WriteLine($" <fg=Yellow>{obj.GetType()}</>");
+                var n = 0;
+                foreach (var x in (IEnumerable<object>)obj)
+                {
+                    Write($"{n++} =>", (depth + 1) * indentationWidth);
+                    printObject(x, depth + 2);
+                }
+            }
+            else if (obj.GetType().IsClass)
+            {
+                WriteLine($" <fg=Yellow>{obj.GetType()}</>");
+                if (depth < maxDepth)
+                {
+                    foreach (var property in obj.GetType().GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance))
+                    {
+                        Write($"{property.Name}:", (depth + 1) * indentationWidth);
+                        printObject(property.GetValue(obj), depth + 1);
+                    }
+                    foreach (var member in obj.GetType().GetMembers(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance))
+                    {
+                        if (member.MemberType != MemberTypes.Field && member.MemberType != MemberTypes.Property)
+                            continue;
+                        Write($"{member.Name}:", (depth + 1) * indentationWidth);
+                        if (member is FieldInfo)
+                        {
+                            printObject(((FieldInfo)member).GetValue(obj), depth + 1);
+                        }
+                        else
+                        {
+                            WriteLine($" <fg=Yellow>{obj.GetType()}</>");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                WriteLine($" <fg=Yellow>{obj.GetType()}</>");
             }
         }
 
